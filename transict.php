9f899574bee04050fd62a894a147b4f4f28ebe61
tejal29may <85352094+tejal29may@users.noreply.github.com>
2021-12-05 17:51:26 +0530

    Create transict.php

---
+++ transict.php
@@ -0,0 +1,178 @@
+<?php
+include('dbconnect.php');
+
+if(isset($_POST['submit']))
+{
+    $from = $_GET['id'];
+    $to = $_POST['to'];
+    $amount = $_POST['amount'];
+
+    $sql = "SELECT * from users_web where id=$from";
+    $query = mysqli_query($conn,$sql);
+    $sql1 = mysqli_fetch_array($query); 
+
+    $sql = "SELECT * from users_web where id=$to";
+    $query = mysqli_query($conn,$sql);
+    $sql2 = mysqli_fetch_array($query);
+
+
+
+   
+    if (($amount)<0)
+   {
+        echo '<script type="text/javascript">';
+        echo ' alert("Oops! Negative values cannot be transferred")';  
+        echo '</script>';
+    }
+
+
+  
+  
+    else if($amount > $sql1['balance']) 
+    {
+        
+        echo '<script type="text/javascript">';
+        echo ' alert("Bad Luck! Insufficient Balance")';  
+        echo '</script>';
+    }
+    
+
+
+    
+    else if($amount == 0){
+
+         echo "<script type='text/javascript'>";
+         echo "alert('Oops! Zero value cannot be transferred')";
+         echo "</script>";
+     }
+
+
+    else {
+        
+               
+                $newbalance = $sql1['balance'] - $amount;
+                $sql = "UPDATE users_web set balance=$newbalance where id=$from";
+                mysqli_query($conn,$sql);
+             
+
+               
+                $newbalance = $sql2['balance'] + $amount;
+                $sql = "UPDATE users_web set balance=$newbalance where id=$to";
+                mysqli_query($conn,$sql);
+                
+                $sender = $sql1['fullname'];
+                $receiver = $sql2['fullname'];
+                $sql = "INSERT INTO transaction(`sender`, `receiver`, `balance`) VALUES ('$sender','$receiver','$amount')";
+                $query=mysqli_query($conn,$sql);
+
+                if($query){
+                     echo "<script> alert('Transaction Successful');
+                                     window.location='history.php';
+                           </script>";
+                    
+                }
+
+                $newbalance= 0;
+                $amount =0;
+        }
+    
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>users details</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!-- <link href="style.css" rel="stylesheet"> -->
+<link rel="stylesheet" href="style2.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<style>
+     h1{
+        color:red;
+        text-decoration:underline;
+    }
+    </style>
+<body>
+    <br>
+    <br><br>
+
+
+
+	<div class="container">
+        <center>
+        <h1>Transaction</h1>
+        <center>
+            <?php
+                include("dbconnect.php");
+                $sid=$_GET['id'];
+                $sql = "SELECT * FROM  users_web where id=$sid";
+                $result=mysqli_query($conn,$sql);
+                if(!$result)
+                {
+                    echo "Error : ".$sql."<br>".mysqli_error($conn);
+                }
+                $rows=mysqli_fetch_assoc($result);
+            ?>
+            <form method="post" name="tcredit" class="tabletext" ><br>
+        <div>
+            <table class="table table-striped table-condensed table-bordered">
+                <tr>
+                    <th class="text-center">Id</th>
+                    <th class="text-center">Name</th>
+                    <th class="text-center">Email</th>
+                    <th class="text-center">Balance</th>
+                </tr>
+                <tr>
+                    <td class="py-2"><?php echo $rows['id'] ?></td>
+                    <td class="py-2"><?php echo $rows['fullname'] ?></td>
+                    <td class="py-2"><?php echo $rows['email'] ?></td>
+                    <td class="py-2"><?php echo $rows['balance'] ?></td>
+                </tr>
+            </table>
+        </div>
+        <br>
+        <label style="color:red">Transfer To:</label>
+        <select name="to" class="form-control" required>
+            <option value="" disabled selected>Choose</option>
+            <?php
+                include("dbconnect.php");
+                $sid=$_GET['id'];
+                $sql = "SELECT * FROM users_web where id!=$sid";
+                $result=mysqli_query($conn,$sql);
+                if(!$result)
+                {
+                    echo "Error ".$sql."<br>".mysqli_error($conn);
+                }
+                while($rows = mysqli_fetch_assoc($result)) {
+            ?>
+                <option class="table" value="<?php echo $rows['id'];?>" >
+                
+                    <?php echo $rows['fullname'] ;?> (Balance: 
+                    <?php echo $rows['balance'] ;?> ) 
+               
+                </option>
+            <?php 
+                } 
+            ?>
+            <div>
+        </select>
+        <br>
+        <br>
+            <label>Amount:</label>
+            <input type="number" class="form-control" name="amount" required>   
+            <br><br>
+                <div class="text-center" >
+            <button class="btn btn-primary" name="submit" type="submit" id="btn">Transfer</button>
+            </div>
+        </form>
+    </div>
+
+</body>
+</html>
